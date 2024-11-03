dfed98767fd68e3fdb46d237ac32823bed5fbe58
Vinicius Silva <106930436+vinirex@users.noreply.github.com>
2024-11-03 20:58:44 -0300

    fixing commit

--- html/static/js/script.js
+++ html/static/js/script.js
@@ -1,1 +1,26 @@
-// TODO
+const btnSend = document.getElementById('enviar');
+btnSend.addEventListener('click', defineUser);
+function defineUser(){
+    //função validadar usuário
+};
+ 
+
+function displayCards(){
+    //função para mostar Cards de acordo com as nececidades do usuário)
+    let dashboard = document.querySelector('#dashboard');
+    
+};
+
+function countOverview(){
+    //funçao pega o total de: Mecanicos, Peças disponiveis, Carros em Manutençao para o Overview do Dashboard
+    //let numMecanicos = pegar valores na base de dados ;
+    //let numPecas = pegar valores na base de dados //;
+    //let numCarros = pegar valores na base de dados ;
+    let Mecanicos = document.querySelector('#res-mecanicos-n');
+    let Pecas = document.querySelector('#res-pecas-n');
+    let Carros = document.querySelector('#res-carros-n'); 
+}
+
+function loadAi(){
+    // função que verifica se o usuário digitou na pesquisa e mostra animação de carregamento
+}
