cd0d7d152a0d41484630d3975fe89173806a48fc
Vinicius Silva <106930436+vinirex@users.noreply.github.com>
2024-11-26 21:19:30 -0300

    dashboard and login

--- html/static/js/script.js
+++ html/static/js/script.js
@@ -19,45 +19,57 @@
 })()
 
 
-const btnSend = document.getElementById('enviar');
-btnSend.addEventListener('click', defineUser);
+ const btnSend = document.getElementById('enviar');
+ btnSend.addEventListener('click', defineUser);
 
-function defineUser(){
-    //função validadar usuário editar conforme as permissoes
+function defineUser() {
+  //função validadar usuário editar conforme as permissoes
 };
 
 
 
-function displayCards(){
-    //função para mostar Cards de acordo com as nececidades do usuário)
-    let dashboard = document.querySelector('#dashboard');
-    
+function displayCards() {
+  //função para mostar Cards de acordo com as nececidades do usuário)
+  let dashboard = document.querySelector('#dashboard');
+
 };
 
-function countOverview(){
-    //funçao pega o total de: Mecanicos, Peças disponiveis, Carros em Manutençao para o Overview do Dashboard
-    //let numMecanicos = pegar valores na base de dados ;
-    //let numPecas = pegar valores na base de dados //;
-    //let numCarros = pegar valores na base de dados ;
-    let Mecanicos = document.querySelector('#res-mecanicos-n');
-    let Pecas = document.querySelector('#res-pecas-n');
-    let Carros = document.querySelector('#res-carros-n'); 
+function countOverview() {
+  //funçao pega o total de: Mecanicos, Peças disponiveis, Carros em Manutençao para o Overview do Dashboard
+  //let numMecanicos = pegar valores na base de dados ;
+  //let numPecas = pegar valores na base de dados //;
+  //let numCarros = pegar valores na base de dados ;
+  let Mecanicos = document.querySelector('#res-mecanicos-n');
+  let Pecas = document.querySelector('#res-pecas-n');
+  let Carros = document.querySelector('#res-carros-n');
 }
 
-function loadAi(){
-    // função que verifica se o usuário digitou na pesquisa e mostra animação de carregamento
+function loadAi() {
+  // função que verifica se o usuário digitou na pesquisa e mostra animação de carregamento
 }
 
-function validadeLogin(){
-  window.location.pathname = './html/dashboard-adm.html';
+
+function validadeLogin() {
+  //Valida o Usuário procurando Email e senha em usuários caastrados
+  let email = document.getElementById('emailv').value;
+  let pswrd = document.getElementById('examplePassword').value;
+  let testEmail = "cliente@email.com";
+  let testePswr = "Teste@123"
+
+  if (email == testEmail && pswrd == testePswr) {
+    window.alert('login validado');
+    window.location.pathname = './html/dashboard-adm.html';
+  }
+  else {
+
+  }
 }
 
-function showSignup(){
-let signup = document.querySelector('#signup');
-let login = document.querySelector('#login');
+function showSignup() {
+  let signup = document.querySelector('#signup');
+  let login = document.querySelector('#login');
 
-login.classList.toggle('visually-hidden')
-signup.classList.toggle('visually-hidden')
+  login.classList.toggle('visually-hidden')
+  signup.classList.toggle('visually-hidden')
 }
-
 
